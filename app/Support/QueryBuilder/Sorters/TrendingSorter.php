82dfe17ccc1459feb039bcc3cb1b32069c1dba47
francoism90 <francoism90@users.noreply.github.com>
2021-07-11 14:22:17 +0200

    wip

--- app/Support/QueryBuilder/Sorters/TrendingSorter.php
+++ app/Support/QueryBuilder/Sorters/TrendingSorter.php
@@ -3,12 +3,18 @@
 namespace App\Support\QueryBuilder\Sorters;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
 use Spatie\QueryBuilder\Sorts\Sort;
 
 class TrendingSorter implements Sort
 {
     public function __invoke(Builder $query, bool $descending, string $property): Builder
     {
-        return $query->reorder();
+        return $query
+            ->with('viewers')
+            ->withCount(['viewers' => function (Builder $query) {
+                $query->where('interactions.created_at', '>=', Carbon::now()->subHours(24));
+            }])
+            ->orderByDesc('viewers_count');
     }
 }
