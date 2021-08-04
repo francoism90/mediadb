731bfa8555a16dcb2cb1facb1e78edc718959ca6
francoism90 <francoism90@users.noreply.github.com>
2021-08-04 12:05:25 +0200

    Return VideoResource

--- app/Http/Controllers/Api/Vod/CaptureController.php
+++ app/Http/Controllers/Api/Vod/CaptureController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Api\Vod;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\VideoResource;
 use App\Models\Video;
 use App\Services\VodService;
-use Illuminate\Http\JsonResponse;
 
 class CaptureController extends Controller
 {
-    public function __invoke(Video $video, int $offset): JsonResponse
+    public function __invoke(Video $video, int $offset): VideoResource
     {
         $uri = sprintf('thumb-%s-w160-h90.jpg', $offset * 1000);
 
@@ -17,9 +17,10 @@ class CaptureController extends Controller
             'video' => $video,
         ]);
 
-        return response()->json([
-            'thumb_url' => $url,
-            'offset' => $offset,
+        return (new VideoResource($video))->additional([
+            'data' => [
+                'capture_url' => $url,
+            ],
         ]);
     }
 }
