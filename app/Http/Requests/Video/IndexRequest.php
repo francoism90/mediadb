a22592016d37b31a6a56c7396f41400feaa1628d
francoism90 <francoism90@users.noreply.github.com>
2021-12-02 16:24:15 +0100

    wip

--- app/Http/Requests/Video/IndexRequest.php
+++ app/Http/Requests/Video/IndexRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Video;
 
 use Elegant\Sanitizer\Laravel\SanitizesInput;
 use Illuminate\Foundation\Http\FormRequest;
+use Spatie\ValidationRules\Rules\Delimited;
 
 class IndexRequest extends FormRequest
 {
@@ -16,9 +17,12 @@ class IndexRequest extends FormRequest
 
     public function rules(): array
     {
+        $tagRule = (new Delimited('string'))->max(10)->allowDuplicates();
+
         return [
             'page' => 'nullable|numeric|min:1|max:48',
             'size' => 'nullable|numeric|min:1|max:24',
+            'tags' => [$tagRule],
             'type' => 'nullable|string|in:favorites,following,viewed',
             'query' => 'nullable|string|min:1|max:255',
             'sort' => 'nullable|string|in:name:asc,duration:asc,duration:desc,created:desc,created:asc',
@@ -30,6 +34,7 @@ class IndexRequest extends FormRequest
         return [
             'page' => 'trim|empty_string_to_null',
             'size' => 'trim|empty_string_to_null',
+            'tags' => 'trim|empty_string_to_null',
             'type' => 'trim|empty_string_to_null',
             'query' => 'trim|empty_string_to_null',
             'sort' => 'trim|empty_string_to_null',
