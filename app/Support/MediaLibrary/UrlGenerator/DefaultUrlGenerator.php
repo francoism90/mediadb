a7798a418927af0127199896ff2f9341d559b4da
francoism90 <francoism90@users.noreply.github.com>
2022-02-12 22:51:38 +0100

    Fix media path

--- app/Support/MediaLibrary/UrlGenerator/DefaultUrlGenerator.php
+++ app/Support/MediaLibrary/UrlGenerator/DefaultUrlGenerator.php
@@ -5,7 +5,6 @@ namespace App\Support\MediaLibrary\UrlGenerator;
 use DateTimeInterface;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
-use League\Flysystem\Adapter\AbstractAdapter;
 use Spatie\MediaLibrary\Support\UrlGenerator\BaseUrlGenerator;
 
 class DefaultUrlGenerator extends BaseUrlGenerator
@@ -35,29 +34,14 @@ class DefaultUrlGenerator extends BaseUrlGenerator
         );
     }
 
-    public function getBaseMediaDirectoryUrl()
+    public function getBaseMediaDirectoryUrl(): string
     {
         return $this->getDisk()->url('/');
     }
 
     public function getPath(): string
     {
-        $adapter = $this->getDisk()->getAdapter();
-
-        $cachedAdapter = '\League\Flysystem\Cached\CachedAdapter';
-
-        if ($adapter instanceof $cachedAdapter) {
-            $adapter = $adapter->getAdapter();
-        }
-
-        $pathPrefix = '';
-
-        if ($adapter instanceof AbstractAdapter) {
-            /** @var AbstractAdapter $pathPrefix */
-            $pathPrefix = $adapter->getPathPrefix();
-        }
-
-        return $pathPrefix.$this->getPathRelativeToRoot();
+        return $this->getRootOfDisk().$this->getPathRelativeToRoot();
     }
 
     public function getResponsiveImagesDirectoryUrl(): string
@@ -67,5 +51,10 @@ class DefaultUrlGenerator extends BaseUrlGenerator
         $path = $this->pathGenerator->getPathForResponsiveImages($this->media);
 
         return Str::finish(url($base.$path), '/');
+    }
+
+    protected function getRootOfDisk(): string
+    {
+        return config("filesystems.disks.{$this->getDiskName()}.root").'/';
     }
 }
