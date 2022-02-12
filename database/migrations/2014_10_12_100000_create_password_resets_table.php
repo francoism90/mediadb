d16d1d218b54442e32910ac845abdc706492782f
francoism90 <francoism90@users.noreply.github.com>
2022-02-12 22:19:50 +0100

    Fix migrations

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,10 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up()
     {
@@ -20,9 +21,11 @@ class CreatePasswordResetsTable extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
     public function down()
     {
         Schema::dropIfExists('password_resets');
     }
-}
+};
