ea77aa4afa39848e79d5dbeb867815ec63dfef7c
francoism90 <francoism90@users.noreply.github.com>
2022-02-18 14:54:01 +0100

    Disable throttle for now

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            // 'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
